197d9356f12e97547a725573a2455dd24c0b011a
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-19 19:05:29 +0300

    Make an error message more general.

--- pacwall.sh
+++ pacwall.sh
@@ -302,7 +302,7 @@ main() {
         echo 'Using pactree to generate the graph'
         generate_graph_pactree "$@"
     else
-        echo "Can't find pactree or debtree." >&2
+        echo "Can't find any backends" >&2
         exit 1
     fi
 
