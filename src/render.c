4e2e66abe8d5e23da1b6af71dc6ad70a2f8a14d5
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-08-30 21:22:06 +0300

    Add some flags to twopi invocation.

--- src/render.c
+++ src/render.c
@@ -3,7 +3,14 @@
 
 void render_graph(const struct opts *opts) {
     int errorcode = 0;
-    subprocess(&errorcode, "twopi", "-Tpng", "-o", opts->png_out, opts->gv_out);
+    subprocess(&errorcode,
+               "twopi", "-Tpng",
+               "-Nshape=point",
+               "-Nheight=0.1",
+               "-Nwidth=0.1",
+               "-Earrowhead=normal",
+               "-o", opts->png_out,
+               opts->gv_out);
     if (errorcode) {
         panic("Twopi returned %d.\n", errorcode);
     }
