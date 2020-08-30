e9c0fa6197d9c4d2de26cb57a07a59845331d484
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-08-30 21:09:48 +0300

    Implement config parsing.

--- src/opts.c
+++ src/opts.c
@@ -1,12 +1,31 @@
+#include <unistd.h>
+
 #include "opts.h"
+#include "util.h"
 
 struct opts parse_opts(config_t *cfg) {
-    struct opts defaults = {
+    struct opts opts = {
         .gv_out = "/tmp/pacwall.gv",
         .png_out = "/tmp/pacwall.png"
     };
-    /*TODO*/
-    return defaults;
+
+    /*TODO: respect XDG_CONFIG_HOME*/
+    chdir(getenv("HOME"));
+    FILE *cfg_file = fopen(".config/pacwall/pacwall.conf", "r");
+    if (cfg_file == NULL) {
+        return opts;
+    }
+    if (!config_read(cfg, cfg_file)) {
+        panic("Malformed config (line %d): %s\n",
+              config_error_line(cfg),
+              config_error_text(cfg));
+    }
+    fclose(cfg_file);
+
+    config_lookup_string(cfg, "output.graphviz", &opts.gv_out);
+    config_lookup_string(cfg, "output.png", &opts.png_out);
+
+    return opts;
 }
 
 
