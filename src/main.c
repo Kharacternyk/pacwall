711bd8b5feb899952908157309d348a34d825aaa
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-08-31 19:39:14 +0300

    Move render code into main().

--- src/main.c
+++ src/main.c
@@ -1,18 +1,25 @@
 #include "generate.h"
-#include "render.h"
 #include "opts.h"
 #include "util.h"
 
 int main(int argc, char **argv) {
     config_t cfg;
     config_init(&cfg);
-    struct opts opts = parse_opts(&cfg);
+    const struct opts opts = parse_opts(&cfg);
 
     generate_graph(&opts);
-    render_graph(&opts);
+
+    int errorcode = 0;
+    subprocess(&errorcode,
+               "twopi", "-Tpng",
+               "-o", opts.output_png,
+               opts.output_graphviz);
+    if (errorcode) {
+        panic("Twopi returned %d.\n", errorcode);
+    }
 
     if (opts.hook != NULL) {
-        int errorcode = system(opts.hook);
+        errorcode = system(opts.hook);
         if (errorcode) {
             panic("Hook returned %d.\n", errorcode);
         }
--- src/render.h
+++
@@ -1,8 +0,0 @@
-#ifndef RENDER_H
-#define RENDER_H
-
-#include "opts.h"
-
-void render_graph(const struct opts *opts);
-
-#endif
