98cfe47fece0635de90caba57a99aa03eb1ed20e
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-09-07 15:11:23 +0300

    Run hook only if argv[0] is *pacwall-hook.

--- src/main.c
+++ src/main.c
@@ -5,11 +5,16 @@
 int main(int argc, char **argv) {
     const struct opts opts = parse_opts();
     chdir_xdg("XDG_CACHE_HOME", ".cache/", "pacwall");
-    generate_graph(&opts);
 
-    subprocess_wait(subprocess_begin("twopi", "-Tpng",
-                                     "-o", "pacwall.png",
-                                     "pacwall.gv"), "twopi");
+    /* If invoked as `*pacwall-hook`, just jump straightly to the hook. */
+    const char *slash = strrchr(argv[0], '/');
+    const char *basename = slash ? slash + 1 : argv[0];
+    if (strcmp(basename, "pacwall-hook")) {
+        generate_graph(&opts);
+        subprocess_wait(subprocess_begin("twopi", "-Tpng",
+                                         "-o", "pacwall.png",
+                                         "pacwall.gv"), "twopi");
+    }
 
     if (opts.hook != NULL) {
         setenv("W", "./pacwall.png", 1);
