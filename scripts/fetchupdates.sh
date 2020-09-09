1d43f173e4379fad449169f91591748ac5b9515b
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-09-09 16:28:59 +0300

    Sync our db with the system one before fetching.

--- scripts/fetchupdates.sh
+++ scripts/fetchupdates.sh
@@ -10,5 +10,8 @@ mkdir -p "$FAKEDB/sync"
 # Link local packages.
 ln -s "$PACMANDB/local/" "$FAKEDB/" &> /dev/null || true
 
+# Copy the system db in case it's newer.
+cp -u "$PACMANDB/sync/"*.db "$FAKEDB/sync/"
+
 # Fetch fresh sync databases.
 fakeroot -- pacman -Sy --dbpath "$FAKEDB" --logfile /dev/null &> /dev/null
